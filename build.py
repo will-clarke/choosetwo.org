e423d056d5ca39e0dff41caeea2a4b30c85af2fb
Will Clarke <wmmclarke@gmail.com>
2024-04-10 11:10:54 +0100

    Python is fun... and better yaml

--- build.py
+++ build.py
@@ -1,7 +1,14 @@
 import jinja2
 
 environment = jinja2.Environment()
-template = environment.from_string("Hello, {{ name }}!")
-out = template.render(name="World")
+template_string = open("template.html").read()
+template = environment.from_string(template_string)
 
-print(out)
+index = template.render(
+    h2="Life is short, make a conscious decision how to spend it",
+    corner_top="Career",
+    corner_right="Hobbies",
+    corner_left="Family",
+)
+
+print(index)
